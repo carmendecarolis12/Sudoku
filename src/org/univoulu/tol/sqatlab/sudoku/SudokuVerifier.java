2f2d66c1ea710c8ed5b67fe8027c76c5f9e207f0
carmendecarolis12 <c.decarolis1@studenti.uniba.it>
2016-11-24 17:25:15 +0100

    implementare tutte le colonne e le sotto matrici

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -1,9 +1,59 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
+	private static boolean verifyRows(int[] arrayint) {
+		
+			int[] xx = new int[9];
+			int j=0;
+		for (int i = 0; i < arrayint.length; i++) {
+			if(i%(float)9 == 0) {
+				j=0;
+				 xx = new int[9];
+			}
+			for (int x : xx) {
+				if(arrayint[i] == x) {
+					return false;
+				}
+			}
+			xx[j++] = arrayint[i];
+		}
+		return true;
+	}
 
-	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
-		return 0;
+	public static boolean verifyColumn(int[] arrayint) {
+		int[] yy = new int[9];
+		int j=0;
+		
+		for (int i = 0; i < arrayint.length; i++) {
+			if(i%(float)9 == 0) {
+				
+				for (int y : yy) {
+					if(arrayint[i] == y) {
+						return false;
+					}
+				}
+				yy[j] = arrayint[i];
+
+				j++;
+			}
+		}
+		return true;
+	}
+	
+	public static int verify(String candidateSolution) {
+		int[] arrayint = new int[candidateSolution.length()];
+		
+		for (int i = 0; i < arrayint.length; i++) {
+			arrayint[i] = Character.getNumericValue(candidateSolution.charAt(i));
+		}
+		
+		 if(!SudokuVerifier.verifyRows(arrayint)) {
+			 return -3;
+		 }
+		 
+		 else if(!SudokuVerifier.verifyColumn(arrayint)) {
+			 return -4;
+		 } 
+		 return 0;
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -5,10 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	public void testFirst3NumberInRecord() {
+		String sudoku = "643";
+		assertEquals(0, SudokuVerifier.verify(sudoku));
 	}
-
+	
+	public void testFirstRecord() {
+		String sudoku2 = "123456789";
+		assertEquals(0, SudokuVerifier.verify(sudoku2));
+	}
+	
+	@Test
+	public void testAllRecords() {
+		String sudoku2 = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		assertEquals(0, SudokuVerifier.verify(sudoku2));
+	}
+	
+	@Test
+	public void testAllRecordsWrong() {
+		String sudoku2 = "417369825632158947958724316825437169791586432346912758289643571573291684164875299";
+		assertEquals(-3, SudokuVerifier.verify(sudoku2));
+	}
+	
+	@Test
+	public void testFirstcolumn() {
+		String sudoku2 = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		assertEquals(0, SudokuVerifier.verify(sudoku2));
+	}
+	@Test
+	public void testAllcolumn() {
+		fail(); //da implementare
+	}
+	@Test
+	public void testAllcolumnWrong() {
+		String sudoku2 = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		assertEquals(-4, SudokuVerifier.verify(sudoku2));
+	}
 }
